72321f232616873a5f30f5a7c71218cd2a302c72
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-02-22 19:30:11 +0545

    adding some file and deleting some file

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("Hello World!!!");
+
+var a = 25;
+
+function hello() {
+    console.log(a);
+}
+console.log(a);
+
+// Hoisting
+// console.log(greeting);
+
+// var greeting = "hello"
+
+// var, let, const
+// const number = 5;
+// console.log(num);
+
---
+++ c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // printf("Hello world!!!, I am Tony Stark Jr.\n");
+    int a=4,b=5;
+    // printf("%d\n",a);
+    // printf("value of a is %d\n",a);
+    printf("Sum of %d and %d is %d\n",a,b,a+b);
+
+}

d8f4ac2adf7afd995fbdc806673342fd56ca6dd2
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-02-23 14:55:10 +0545

    adding a new file

---
+++ array.js
@@ -0,0 +1,9 @@
+// Arrays and Loops 
+// const languages = ['javascript', 'python', 'c']
+// length
+// console.log(languages.length);
+// console.log(languages.push('dart'));
+// console.log(languages.unshift('java'));
+// console.log(languages.pop());
+// console.log(languages.shift());
+// console.log(languages);

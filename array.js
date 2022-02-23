b3fd34990a4382102a674e0757a48944c20b815c
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-02-23 19:41:20 +0545

    modifying array.js

--- array.js
+++ array.js
@@ -7,3 +7,50 @@
 // console.log(languages.pop());
 // console.log(languages.shift());
 // console.log(languages);
+
+// Loops
+// const actors = [
+//     {
+//         name: "actor1",
+//         payment: 100
+//     },
+//     {
+//         name: "actor2",
+//         payment: 200
+//     },
+//     {
+//         name: "actor3",
+//         payment: 300
+//     }
+// ]
+
+// Confusing code for me
+
+// for (let i = 0; i < actors.length; i++) {
+//     actors[i].payment = actors[i].payment - 20;
+// }
+
+// console.log(actors);
+
+const actors = [
+    {
+        name: "actor1",
+        payment: 100
+    },
+    {
+        name: "actor2",
+        payment: 200
+    },
+    {
+        name: "actor3",
+        payment: 300
+    }
+]
+
+
+// ForEach Loops
+actors.forEach((actors) => {
+    actors.payment=actors.payment-30;
+    console.log(actors.payment);
+    // console.log(actors);
+})

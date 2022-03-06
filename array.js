a3edaa2de70f5cfd9187b8c889f790dbcadbc680
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-03-06 17:25:47 +0545

    learning array and its method and also adding mdn doc code while reading mdn doc

--- array.js
+++ array.js
@@ -32,25 +32,152 @@
 
 // console.log(actors);
 
-const actors = [
-    {
-        name: "actor1",
-        payment: 100
-    },
-    {
-        name: "actor2",
-        payment: 200
-    },
-    {
-        name: "actor3",
-        payment: 300
-    }
-]
+// ]const actors = [
+//     {
+//         name: "actor1",
+//         payment: 100
+//     },
+//     {
+//         name: "actor2",
+//         payment: 200
+//     },
+//     {
+//         name: "actor3",
+//         payment: 300
+//     }
+// ]
 
 
 // ForEach Loops
-actors.forEach((actors) => {
-    actors.payment=actors.payment-30;
-    console.log(actors.payment);
-    // console.log(actors);
-})
+// actors.forEach((actors) => {
+//     actors.payment=actors.payment-30;
+//     console.log(actors.payment);
+//     // console.log(actors);
+// })
+
+// For let of Loops
+
+
+// Filter 
+// const students = [
+//     {
+//         name: 'Student 1',
+//         marks: 45
+//     },
+//     {
+//         name: 'Student 2',
+//         marks: 60
+//     },
+//     {
+//         name: 'Student 3',
+//         marks: 35
+//     }
+// ]
+
+// const failed = students.filter((students => {
+//     return students.marks < 45 
+// }))
+
+// console.log(failed);
+
+
+
+// Map            
+// const users = [{
+//     fname: 'Tony',
+//     lname: 'Stark'
+// },
+// {
+//     fname: 'Santosh',
+//     lname: 'Marar'
+// }
+// ]
+
+// const maps = users.map((user) => {
+//     console.log(` Full Name : ${user.fname} ${user.lname}`);
+// })
+
+
+// Reduce
+// const movies = [
+//     {
+//         name: 'Insterstellar',
+//         budget: 100
+//     },
+//     {
+//         name: 'Social',
+//         budget: 150
+
+//     },
+//     {
+//         name: 'Matrix',
+//         budget: 300
+//     }
+// ]
+
+// let total = 0;
+
+// movies.forEach((movie) => {
+//     total += movie.budget;
+
+// })
+
+
+// const total = movies.reduce((acc, movie) => {
+//     acc += movie.budget
+//     return acc
+// }, 0)
+
+// console.log(total)
+
+
+// indexOf
+// admins = [2, 5, 4]
+
+// const user = {
+//     name: 'xyz',
+//     id: 5
+// }
+
+// const isAdmin = admins.indexOf(user.id) > -1;
+// console.log(isAdmin);
+
+
+// includes
+// console.log(admins.includes(user.id));
+
+
+// find
+// const peoples = [
+//     {
+//         id: 1,
+//         name: "Santosh Marar",
+//         class: 11,
+//         fav_subject: ["computer", "math", "physics", "chemistry", "economic", "history"]
+//     },
+//     {
+//         id: 2,
+//         name: "Tony Stark",
+//         class: "unknown",
+//         fav_things: ['flying', 'cars']
+//     },
+// ]
+
+// const fai = peoples.find((people) => {
+//     if (people.name === "Santosh Marar") {
+//         console.log(people.id,people.fav_subject);
+//     }
+// })
+
+
+
+// sort
+const people = ["Santosh", "Suman", "Suraj", "Saket", "Dipak", "Tony", "Stark"]
+const peoples=people.sort()
+console.log(peoples);
+
+
+// slice
+// const people = ["Santosh", "Suman", "Suraj", "Saket", "Dipak", "Tony", "Stark"]
+// const peoples=people.slice(1)
+// console.log(peoples);

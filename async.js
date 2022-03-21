dbe559d0132f89c3ef718f309851d9de61f7eb01
Tony Stark Jr <st.juniordeveloper@gmail.com>
2022-03-21 09:01:10 +0545

    adding async

---
+++ async.js
@@ -0,0 +1,9 @@
+//    Asynchronous JavaScript
+
+console.log('hello');
+
+setTimeout(() => {
+    console.log("I am from timeout");
+}, 3000);
+
+console.log('JavaScript');
--- functions.js
+++ functions.js
@@ -27,3 +27,55 @@
 
 // console.log(area); 
 
+
+// variable scope
+// const fileName = 'xyz.pdf';
+// function download() {
+//     // download logic goes here
+
+//     console.log(fileName);
+// }
+
+// download()
+
+
+// #. Function Declaration
+// function login() {
+
+// }
+
+
+// #. Function Expression
+// const login = function () {
+
+// }
+
+// #. Callback
+
+// function formatText(text, formatCb) {
+//     return typeof formatCb === 'function' ? formatCb(text) : text.toUpperCase();
+// }
+
+// const result = formatText('hello', (text) => text.charAt(0).toUpperCase() + text.slice(1))
+// console.log(result);
+
+
+// #. IIFE ( Immediately invoked function expression )
+// (function setup() {
+//     console.log("Setup done!");
+// })();
+
+
+// #. Arrow function
+
+// const login = () => {
+//     console.log("It is done");
+// }
+// login();
+
+
+// const sum = (num1, num2) => {
+//     return num1 + num2;
+// }
+// const result = sum(1,5)
+// console.log(result);
